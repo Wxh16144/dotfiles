0f82f6f6163fb4fd68d9d36da8cc8bcd7e2bac9a
wuxh <wxh1220@gmail.com>
2023-03-23 09:57:19 +0800

    feat: use @wuxh/backup-cli
    remove mackup

---
+++ scripts/sync.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const path = require('path');
+const { execSync } = require('child_process');
+
+process.env.BACKUP_CONFIG_FILE = path.resolve(__dirname, '../.backuprc');
+process.env.BACKUP_CUSTOM_APP_DIR = path.resolve(__dirname, '../.backup');
+
+
+const backupCli = require.resolve('@wuxh/backup-cli');
+
+const args = process.argv.slice(2);
+execSync(`node ${backupCli} ${args.join(' ')}`, { stdio: 'inherit' });
