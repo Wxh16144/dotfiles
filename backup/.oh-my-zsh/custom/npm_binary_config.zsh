292bd46e491fb5cf64ddd9d8b9880f800c824e2a
wuxh <wxh1220@gmail.com>
2023-09-05 09:23:28 +0800

    chore: fix puppeteer registry env

--- backup/.oh-my-zsh/custom/npm_binary_config.zsh
+++ backup/.oh-my-zsh/custom/npm_binary_config.zsh
@@ -10,7 +10,6 @@ export ELECTRON_BUILDER_BINARIES_MIRROR="https://cdn.npmmirror.com/binaries/elec
 export SASS_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-sass"
 export SWC_BINARY_SITE="https://cdn.npmmirror.com/binaries/node-swc"
 export NWJS_URLBASE="https://cdn.npmmirror.com/binaries/nwjs/v"
-export PUPPETEER_DOWNLOAD_HOST="https://cdn.npmmirror.com/binaries"
 export SENTRYCLI_CDNURL="https://cdn.npmmirror.com/binaries/sentry-cli"
 export SAUCECTL_INSTALL_BINARY_MIRROR="https://cdn.npmmirror.com/binaries/saucectl"
 export npm_config_sharp_binary_host="https://cdn.npmmirror.com/binaries/sharp"
@@ -18,3 +17,9 @@ export npm_config_sharp_libvips_binary_host="https://cdn.npmmirror.com/binaries/
 export npm_config_robotjs_binary_host="https://cdn.npmmirror.com/binaries/robotj"
 # For Cypress >=10.6.0, https://docs.cypress.io/guides/references/changelog#10-6-0
 export CYPRESS_DOWNLOAD_PATH_TEMPLATE='https://cdn.npmmirror.com/binaries/cypress/${version}/${platform}-${arch}/cypress.zip'
+
+# https://zhuanlan.zhihu.com/p/637107614
+# puppeteer<=19
+export PUPPETEER_DOWNLOAD_HOST="https://cdn.npmmirror.com/binaries"
+# puppeteer>20.1+
+export PUPPETEER_DOWNLOAD_BASE_URL="https://cdn.npmmirror.com/binaries/chrome-for-testing"
