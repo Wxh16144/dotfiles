d2d8aec68bc8fa321bfad56568553a73ff4353c3
wuxh <wxh1220@gmail.com>
2024-02-19 17:35:06 +0800

    Revert "feat: add private env(desensitize)"
    This reverts commit 813628e004e7039a993528678408ba2a3413cc67.

--- backup/.oh-my-zsh/custom/private_env.zsh
+++
@@ -1,7 +0,0 @@
-export GITHUB_TOKEN_PUBLISH=ghp_xxxxxx
-export CHAT_OPENAI_TOKEN=sk-xxxxxx
-export GITHUB_COPILOT_PLUGIN_KEY=ghu_xxxxxx
-export NPMJS_TOKEN=npm_xxxxxx
-
-# company
-export COMPANY_NPM_TOKEN=xxxxxx
