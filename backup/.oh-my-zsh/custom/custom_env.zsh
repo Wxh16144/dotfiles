3cd85f51f675d815c318b1823cb8c1a1bc2dba1e
wuxh <wxh1220@gmail.com>
2024-02-28 10:36:53 +0800

    chore: update

--- backup/.oh-my-zsh/custom/custom_env.zsh
+++ backup/.oh-my-zsh/custom/custom_env.zsh
@@ -13,8 +13,10 @@ export HISTSIZE='32768';
 export HISTFILESIZE="${HISTSIZE}";
 
 # my projects directory
-export USER_CONFIG="$HOME/Config"
+export CONFIG="$HOME/Config"
+export SCRIPTS="$HOME/Scripts"
 export WORKSPACE="$HOME/Code"
+# code directory
 export COMPANY=$WORKSPACE/company
 export OSS=$WORKSPACE/oss
 export MY=$WORKSPACE/self
@@ -32,3 +34,25 @@ export BACKUP_REMOTE_NAME="backup"
 # https://apple.stackexchange.com/a/22716
 # https://iboysoft.com/wiki/tmp-folder-mac.html
 # export OSX_TMPDIR=$TMPDIR
+
+
+# ====== footer ====== #
+function __internal_ensure_dir() {
+  local dirs=(
+    $CONFIG
+    $SCRIPTS
+    $WORKSPACE
+    $COMPANY
+    $OSS
+    $MY
+    $PLAY
+  )
+
+  for dir in ${dirs[@]}; do
+    if [ ! -d $dir ]; then
+      mkdir -p $dir
+    fi
+  done
+}
+
+__internal_ensure_dir
