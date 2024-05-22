669b60ce9c79660552531e529f2506814d243b9f
wuxh <wxh1220@gmail.com>
2024-05-22 16:09:26 +0800

    chore: update

--- backup/.oh-my-zsh/custom/brew_source.zsh
+++ backup/.oh-my-zsh/custom/brew_source.zsh
@@ -5,3 +5,7 @@ export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
 export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/bottles"
+
+# node-canvas、node-gyp
+# ref: https://github.com/Automattic/node-canvas?tab=readme-ov-file#compiling
+# brew install pkg-config cairo pango libpng jpeg giflib librsvg pixman
--- backup/.oh-my-zsh/custom/temp-shortcut.zsh
+++ backup/.oh-my-zsh/custom/temp-shortcut.zsh
@@ -1,35 +1,15 @@
 # 一些临时的快捷方式
 
-
-# GitHub 组织 https://github.com/lobehub
-
-autoload -U add-zsh-hook
-
-# 写的比较粗糙，可以自己决定逻辑
-lobehub__detect_expected_project() {
-  local project_name=${1:-"lobehub-monorepo"}
-
-  if [[ $(basename "$PWD") != "$project_name" ]]; then
-    return 1
-  fi
-
-  if [[ $(git remote -v | grep "$project_name") == "" ]]; then
-    return 1
-  fi
-
-  return 0
-}
-
-# 将 lobehub-monorepo 总是保持最新状态
-lobehub__always_latest() {
-  if lobehub__detect_expected_project && [[ $(git status -s) == "" ]]; then
-    echo "Auto Updating lobehub-monorepo..."
-    git pull --recurse-submodules
-  fi
-}
-
-add-zsh-hook chpwd lobehub__always_latest
-lobehub__always_latest
-
 # 公司产品原型
 export COM_AXURE=$COMPANY/axure 
+alias com0="http-server $COMPANY/axure -p 10086 -c-1 -o"
+
+# Savery 编辑器
+alias com1="cd $COMPANY/savery-editor" 
+alias com1c="cd $COMPANY/saveryconfig" # 配置项
+
+# 哥伦布编辑器
+alias com2="cd $COMPANY/columbus-editor" 
+alias com2c="cd $COMPANY/columbus-config" # 配置项
+
+
