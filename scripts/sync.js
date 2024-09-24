3da55d5355c349e97b40884bcbd79ac5f1d570e9
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-09-24 13:16:45 +0800

    chore: update script

--- scripts/sync.js
+++ scripts/sync.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 
 process.env.BACKUP_CONFIG_FILE = path.resolve(__dirname, '../.backuprc');
 process.env.BACKUP_CUSTOM_APP_DIR = path.resolve(__dirname, '../.backup');
-process.env.BACKUP_UPSTREAM_HOME = '/Users/wuxh' // need >= 1.9.0
+process.env.BACKUP_UPSTREAM_HOME ||= '/Users/wuxh' // need >= 1.9.0
 
 const backupCli = require.resolve('@wuxh/backup-cli');
 
