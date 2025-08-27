13158a0257943cb48101eb1f6bca46bddc2edaff
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-08-27 12:42:49 +0800

    feat: add clean_nvm_versions command

--- backup/.oh-my-zsh/custom/nvm_custom.zsh
+++ backup/.oh-my-zsh/custom/nvm_custom.zsh
@@ -1,7 +1,7 @@
 # https://github.com/nvm-sh/nvm#manual-install
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 # https://github.com/nvm-sh/nvm#deeper-shell-integration
 autoload -U add-zsh-hook
@@ -39,4 +39,54 @@ export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
 # https://www.voidcanvas.com/global-module-not-found-when-node-version-is-changed-using-nvm/
 function nvm_install() {
   nvm install $1 --reinstall-packages-from=default --skip-default-packages
+}
+
+# 清理 nvm 已安装的 Node.js 版本，只保留每个 major 版本的最新版本
+# like: https://github.com/nvm-sh/nvm/issues/1195
+function clean_nvm_versions() {
+  # 适用 macos。ai 生成的
+  local versions=($(nvm ls --no-colors | awk '/^ *v[0-9]+\.[0-9]+\.[0-9]+/ {gsub(/^ +/, "", $1); print $1}'))
+
+  typeset -A latest_versions
+  for v in $versions; do
+    local major=$(echo $v | cut -d. -f1 | tr -d 'v')
+    latest_versions[$major]=$v
+  done
+
+  local to_delete=()
+  local to_keep=()
+  for v in $versions; do
+    local major=$(echo $v | cut -d. -f1 | tr -d 'v')
+    if [[ $v != ${latest_versions[$major]} ]]; then
+      to_delete+=($v)
+    else
+      to_keep+=($v)
+    fi
+  done
+
+  # 打印所有版本
+  for v in $versions; do
+    local major=$(echo $v | cut -d. -f1 | tr -d 'v')
+    if [[ $v != ${latest_versions[$major]} ]]; then
+      print_red "$v (delete)"
+    else
+      print_green "$v (keep)"
+    fi
+  done
+
+  if ((${#to_delete[@]} > 0)); then
+    echo -n "Do you want to delete the red versions above? [y/N]: "
+    read -r answer
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+      for v in "${to_delete[@]}"; do
+        print_yellow "Uninstalling $v ..."
+        nvm uninstall $v
+      done
+      print_green "Done."
+    else
+      print_red "Aborted. No versions deleted."
+    fi
+  else
+    print_yellow "No versions to delete."
+  fi
 }
